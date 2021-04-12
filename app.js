e64c40a257799b09c93438ccea1c34182682b8df
Fernandes <rodrigo.a.fernandes@capgemini.com>
2021-04-12 15:47:33 -0300

    cadastro de brasileiro serie b

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const { json } = require('body-parser');
 const app = require('./config/app');
 const controller = require('./src/controller/buscarDadosSimulacao');
 
-app.get('/', async function(req,res) {
+app.get('/brasileirao-serie-a-2021', async function(req,res) {
 	const campeonato = await controller.obterCampeonato("BRA2021A");
 	const times = await controller.obterTimes("BRA2021A");
 	const rodadas = await controller.obterRodadas("BRA2021A");
+	
+	res.render("simulador", 
+	{
+		campeonato: campeonato,
+		times: times,
+		rodadas: rodadas
+	});
+});
+
+app.get('/brasileirao-serie-b-2021', async function(req,res) {
+	const campeonato = await controller.obterCampeonato("BRA2021B");
+	const times = await controller.obterTimes("BRA2021B");
+	const rodadas = await controller.obterRodadas("BRA2021B");
 	
 	res.render("simulador", 
 	{
