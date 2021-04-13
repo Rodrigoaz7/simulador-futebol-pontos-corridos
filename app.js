87ad42e9f14f98ca9c1bc12f7e40771a984cba49
Fernandes <rodrigo.a.fernandes@capgemini.com>
2021-04-13 10:03:32 -0300

    adicionando tela inicial para adentrar em campeoantos

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const { json } = require('body-parser');
 const app = require('./config/app');
 const controller = require('./src/controller/buscarDadosSimulacao');
 
+app.get('/', async function(req,res) {
+	res.render("index", {});
+});
+
 app.get('/brasileirao-serie-a-2021', async function(req,res) {
 	const campeonato = await controller.obterCampeonato("BRA2021A");
 	const times = await controller.obterTimes("BRA2021A");
