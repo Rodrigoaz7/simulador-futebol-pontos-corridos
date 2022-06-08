a16c28ae3c9b59aa4c1bc442054799624e47cde5
Rodrigo Azevedo <Rodrigo>
2022-06-08 10:39:38 -0300

    install mvn

--- Scripts/after_install.sh
+++ Scripts/after_install.sh
@@ -3,5 +3,5 @@ cp -R /home/ec2-user/app/* /var/www/html/
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install 16
 nvm use 16
