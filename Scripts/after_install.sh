8ce3aa7fc489f5b69305b6e25c15a422148d46fa
Rodrigo Azevedo <Rodrigo>
2022-06-08 10:24:58 -0300

    appspec

--- Scripts/after_install.sh
+++ Scripts/after_install.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-set -e
 cp -R /home/ec2-user/app/* /var/www/html/   
-cd /var/www/html/  
-npm start
--- Scripts/app_start.sh
+++ Scripts/app_start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 systemctl start httpd.service
 systemctl enable httpd.service
+set -e
+nvm use 16
+cd /var/www/html/  
+npm start
