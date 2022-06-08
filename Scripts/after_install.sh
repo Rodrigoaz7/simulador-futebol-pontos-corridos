b5c11aa148312edac002f8183f49676ccdc5db75
Rodrigo Azevedo <Rodrigo>
2022-06-08 10:35:59 -0300

    install mvn

--- Scripts/after_install.sh
+++ Scripts/after_install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 cp -R /home/ec2-user/app/* /var/www/html/   
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+. ~/.nvm/nvm.sh
+nvm install node
+nvm use 16
--- Scripts/app_start.sh
+++ Scripts/app_start.sh
@@ -2,6 +2,5 @@
 systemctl start httpd.service
 systemctl enable httpd.service
 set -e
-nvm use 16
 cd /var/www/html/  
 npm start
